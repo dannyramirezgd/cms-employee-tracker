d329a14ac042745f08630bdf9c0ba3e2a62449db
dannyrgd <danny.ramirezgd@gmail.com>
2022-02-12 11:06:43 -0500

    consolidated tables and data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,39 +1,35 @@
 INSERT INTO department (department_name)
 VALUES
-    ('Sales'),
-    ('Engineering'),
+    ('Marketing'),
+    ('Human Resources'),
     ('Finance'),
     ('Legal'),
-    ('IT');
+    ('Information Technology');
 
-INSERT INTO salary (salary_value)
+
+INSERT INTO roles (role_name, department_id, salary)
 VALUES
-    ('10000'),
-    ('40000'),
-    ('60000'),
-    ('90000'),
-    ('150000'),
-    ('100000000');
+    ('Marketing Specialist', 1, 50000),
+    ('Marketing research analyst', 1, 65000),
+    ('Recruiter', 2, 55000),
+    ('HR Manager', 2, 75000),
+    ('Financial Advisor', 3, 100000),
+    ('Accountant', 3, 150000),
+    ('Junior Lawyer', 4, 100500),
+    ('Senior Partner', 4, 250000),
+    ('Entry Developer', 5, 80000),
+    ('Senior Developer', 5, 95000);
 
-INSERT INTO roles (role_name, department_id, salary_id)
-VALUES
-    ('Salesperson', 1, 1),
-    ('Entry Engineer', 2, 2),
-    ('Software Engineer', 2, 3),
-    ('Lead Engineer', 2, 3),
-    ('Lawyer', 4, 5),
-    ('Financial Advisor', 3, 4),
-    ('CFO', 3, 6),
-    ('CEO', 1, 6),
-    ('CIO', 2, 6),
-    ('IT Help Desk', 5, 6);
-
-INSERT INTO employee (first_name, last_name, role_id, department_id, salary_id, manager)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 -- these are the values inserted by the constructor based on input
-    ('Bill', 'Jenkins', 1, 1, 1, null),
-    ('Andy', 'Lilo', 2, 2, 2, null),
-    ('Becky', 'Bouchard', 3, 2, 3, null),
-    ('Ellen', 'Paige', 4, 2, 3, null),
-    ('Hughbert', 'Figsworth', 5, 4, 5, null),
-    ('Eleanor', 'Rigby', 6, 3, 4, null);
+    ('Bill', 'Jenkins', 1, null),
+    ('Andy', 'Lilo', 2, null),
+    ('Becky', 'Bouchard', 3, null),
+    ('Ellen', 'Paige', 4, null),
+    ('Hughbert', 'Figsworth', 5, null),
+    ('Eleanor', 'Rigby', 6, null),
+    ('Adam', 'Driver', 7, null),
+    ('Chris', 'Devine', 8, null),
+    ('Andrew', 'Filhouse', 9, null),
+    ('John', 'Kennedy', 10, null);
