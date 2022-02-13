44f44434737e68194f8fa7f9207d93fe2fb88781
dannyrgd <danny.ramirezgd@gmail.com>
2022-02-13 00:16:16 -0500

    can create new department and new role

--- index.js
+++ index.js
@@ -1,82 +1,10 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const Organization = require('./lib/Organization');
-const db = require('./db/connection');
-//const getFromDatabase = require('./lib/Organization');
+const viewFromDatabase = require('./lib/viewRequests')
+const { addToDatabase, deleteFromDatabase } = require('./lib/crudRequests')
 
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
-
-
-const viewFromDatabase = table => {
-    if(table === 'department'){
-        return db.promise().query(`SELECT * FROM department`)
-    } else if (table === 'roles') {
-        return db.promise().query(
-            `SELECT role_id as id, 
-            role_name as 'Job Title', 
-            salary, 
-            department.department_name as 'Department Name'
-            FROM roles
-            LEFT JOIN department 
-            ON roles.department_id = department.department_id`)
-    } else if (table === 'employees') {
-        return db.promise().query(
-            `SELECT first_name as 'First Name',
-            last_name as 'Last Name',
-            roles.role_name as 'Job Title',
-            department.department_name as 'Department Name',
-            roles.salary as Salary,
-            manager_id as Manager
-            FROM employee
-            LEFT JOIN roles
-            ON employee.role_id = roles.role_id
-            LEFT JOIN department
-            ON roles.department_id = department.department_id
-            `)
-    } else if (table === 'managers'){
-        return db.promise().query(
-            `SELECT A.employee_id as id, 
-            A.first_name as 'First Name', 
-            A.last_name as 'Last Name', 
-            B.first_name as 'Manager First Name', 
-            B.last_name as 'Manager Last Name'
-            FROM employee A
-            LEFT JOIN employee B on A.manager_id = B.employee_id
-            ORDER BY A.manager_id DESC;
-        `)
-    } else if (table === 'emplyByDept'){
-        return db.promise().query(
-            `SELECT employee.employee_id as id,
-            employee.first_name as 'First Name',
-            employee.last_name as 'Last Name',
-            department.department_name as Department
-            FROM employee
-            LEFT JOIN roles ON employee.role_id = roles.role_id
-            LEFT JOIN department ON roles.department_id = department.department_id
-            ORDER BY department.department_id 
-
-            `)
-    }
-}
-
-const addToDatabase = async item => {
-    if (item === 'addToDepartment') {
-        const sql = `INSERT INTO department (department_name) VALUES (?)`
-        const request = await inquirer.prompt({
-            type:'input',
-            name:'department',
-            message:'What is the name of the Department you wish to add?'
-        });
-        return db.promise().execute(sql, [request.department], (err, result) => {
-            if(err) {
-                console.log('Error')
-            } else {
-                console.log('Success')
-            }
-        })
-    }
-}
 
 const init = async () => {
     console.log (`Please indicate desired request`);
@@ -92,7 +20,7 @@ const init = async () => {
             'View all employees by Manager',
             'View all employees by department',
             'Create a new department',
-            'POST a role',
+            'Create a new role',
             'POST an employee',
             'PUT Employee Role',
             'DELETE department',
@@ -128,6 +56,9 @@ async function requestActions(option) {
             break;
         case 'Create a new department':
             await addToDatabase('addToDepartment')
+            break;
+        case 'Create a new role':
+            await addToDatabase('newRole')
             break;
         default:
             console.log('Try again');
